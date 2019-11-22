40a470c4817b780f43e7bcd91c0596938560be2e
naicsss <56775102+naicsss@users.noreply.github.com>
2019-11-22 14:33:26 -0300

    implemento mongoose

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/signin', (req, res) => {
+    res.send('Cargando app');
+});
+
+router.get('/signup', (req, res) => {
+    res.send('Formulario de authentificacion')
+});
+
 module.exports = router;
