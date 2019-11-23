10634015f7d9effaacb59469068c9678990c9b33
naicsss <56775102+naicsss@users.noreply.github.com>
2019-11-23 10:44:41 -0300

    formulario funciona

--- backend/src/routes/contact.js
+++ backend/src/routes/contact.js
@@ -5,6 +5,26 @@ router.get('/contact/add', (req, res) => {
     res.render('contacts/new-contact');
 });
 
+router.post('/notes/new-note', (req, res) => {
+    const {title, description }= (req.body);
+    const errors = [];
+    if(!title) {
+        errors.push({text: 'Porfavor escriba un titulo'});
+    }
+    if (!description) {
+        errors.push({text: 'Porfavor escriba una nota'});
+    }
+    if(errors.length > 0) {
+        res.render('contacts/new-contact', {
+            errors,
+            title,
+            description
+        });
+        } else {
+            res.send('ok');
+        }
+});
+
 router.get('/notes', (req, res) => {
     res.send('Notes from database');
 });
