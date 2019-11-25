bca534a1a962d7b1cc90d3219713e638a78baeea
naicsss <56775102+naicsss@users.noreply.github.com>
2019-11-25 11:15:11 -0300

    crud listo empiezo con el login

--- backend/src/routes/contact.js
+++ backend/src/routes/contact.js
@@ -25,13 +25,32 @@ router.post('/contacts/new-contact', async (req, res) => {
         } else {
             const newContact = new Contact({ title, description});
             await newContact.save();
+            req.flash('success_msg', 'Contacto agregado!')
             res.redirect('/contacts')
         }
 });
 
 router.get('/contacts', async (req, res) => {    
-    const contact = await Contact.find().sort({date: 'desc'});
-    res.render('contacts/all-contacts', { contact });
+    const contacts = await Contact.find().sort({date: 'desc'});
+    res.render('contacts/all-contacts', { contacts });
+});
+
+router.get('/contacts/edit/:id', async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    res.render('contacts/edit-contact', {contact});
+});
+
+router.put('/contacts/edit-contact/:id', async (req, res) => {
+    const { title, description }= req.body;
+    await Contact.findByIdAndUpdate(req.params.id, { title, description });
+    req.flash('success_msg', 'Contacto Actualizado!');
+    res.redirect('/contacts');
+});
+
+router.delete('/contacts/delete/:id', async (req, res) => {
+    await Contact.findByIdAndDelete(req.params.id)
+    req.flash('success_msg', 'Contacto Eliminado');
+    res.redirect('/contacts');
 });
 
 module.exports = router;
