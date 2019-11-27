e689835c11d1c14bb4c181671dd345229b095e47
naicsss <56775102+naicsss@users.noreply.github.com>
2019-11-27 10:08:44 -0300

    registro terminado

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
 router.get('/users/signin', (req, res) => {
     res.render('users/signin');
 });
 
 router.get('/users/signup', (req, res) => {
     res.render('users/signup')
+});
+
+router.post('/users/signup', async (req, res) => {
+        const { name, email, password, confirm_password} = req.body;
+        const errors = [];
+        if(name.length <= 0) {
+            errors.push({text: 'Porfavor inserta tu nombre'})
+        }
+        if(password != confirm_password) {
+            errors.push({text: 'Las contraseñas no coinciden'});
+        }
+        if (password.length < 4) {
+            errors.push({text: 'La contraseña deberia ser mayor a 4 caracteres'})
+        }
+        if(errors.length > 0){
+            res.render('users/signup', {errors, name, email, password, confirm_password});
+        } else {
+            const emailUser = await User.findOne({email: email});
+            if (emailUser) {
+                req.flash('error_msg', 'El Email ya está en uso');
+                res.redirect('/users/signup');
+            }
+            const newUser = new User({name, email, password});
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            req.flash('success_msg', 'Registro Exitoso');
+            res.redirect('/users/signin');
+        }
 });
 
 module.exports = router;
