033b6f59ecaa80879f85e0e8bd96ff5796c422b9
naicsss <56775102+naicsss@users.noreply.github.com>
2019-12-02 10:53:18 -0300

    app base terminada comienzo modificaciones

--- backend/src/routes/contact.js
+++ backend/src/routes/contact.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const Contact = require('../models/Contact');
+const  {isAuthenticated } = require('../helpers/auth');
 
-router.get('/contacts/add', (req, res) => {
+router.get('/contacts/add', isAuthenticated, (req, res) => {
     res.render('contacts/new-contact');
 });
 
-router.post('/contacts/new-contact', async (req, res) => {
+router.post('/contacts/new-contact', isAuthenticated, async (req, res) => {
     const {title, description }= (req.body);
     const errors = [];
     if(!title) {
@@ -24,30 +25,31 @@ router.post('/contacts/new-contact', async (req, res) => {
         });
         } else {
             const newContact = new Contact({ title, description});
+            newContact.user = req.user.id;
             await newContact.save();
             req.flash('success_msg', 'Contacto agregado!')
             res.redirect('/contacts')
         }
 });
 
-router.get('/contacts', async (req, res) => {    
-    const contacts = await Contact.find().sort({date: 'desc'});
+router.get('/contacts', isAuthenticated, async (req, res) => {    
+    const contacts = await Contact.find({user: req.user.id}).sort({date: 'desc'});
     res.render('contacts/all-contacts', { contacts });
 });
 
-router.get('/contacts/edit/:id', async (req, res) => {
+router.get('/contacts/edit/:id', isAuthenticated, async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     res.render('contacts/edit-contact', {contact});
 });
 
-router.put('/contacts/edit-contact/:id', async (req, res) => {
+router.put('/contacts/edit-contact/:id', isAuthenticated, async (req, res) => {
     const { title, description }= req.body;
     await Contact.findByIdAndUpdate(req.params.id, { title, description });
     req.flash('success_msg', 'Contacto Actualizado!');
     res.redirect('/contacts');
 });
 
-router.delete('/contacts/delete/:id', async (req, res) => {
+router.delete('/contacts/delete/:id', isAuthenticated, async (req, res) => {
     await Contact.findByIdAndDelete(req.params.id)
     req.flash('success_msg', 'Contacto Eliminado');
     res.redirect('/contacts');
